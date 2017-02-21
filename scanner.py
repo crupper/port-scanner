45d8a93e47198347521cfcce071c8574a22c6f94
Chris Rupper <chrisrupper@gmail.com>
2017-02-21 13:04:05 -0700

    Added basic options

--- scanner.py
+++ scanner.py
@@ -4,6 +4,39 @@ from logging import getLogger, ERROR
 getLogger("scapy.runtime").setLevel(ERROR)
 from scapy.all import *
 import sys
+import getopt
 from datetime import datetime
 from time import strftime
 
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "ho:v", ["help", "output="])
+        # Intro message
+        print "Hello!"
+        print "Welcome to this port scanner"
+
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print str(err)  # will print something like "option -a not recognized"
+        # usage()
+        sys.exit(2)
+    output = None
+    verbose = False
+    for o, a in opts:
+        if o == "-v":
+            verbose = True
+        elif o in ("-h", "--help"):
+            # usage()
+            print "Help: "
+            sys.exit()
+        elif o in ("-o", "--output"):
+            output = a
+        else:
+            assert False, "unhandled option"
+    # ...
+    # Take in arguments
+    print "Starting scan of " + sys.argv[1]
+
+if __name__ == "__main__":
+    main()
